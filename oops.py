e32773cb90ddc833fff3c069a0ff0d27ff3a89d6
PARDHA SARADHI MOLLI <113762380+PardhaSaradhimolli@users.noreply.github.com>
2023-03-08 12:47:57 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,38 @@
+'''class happy:
+    a=100
+    def method(self):
+        b=12
+        print(self.a)
+        print(b)
+obj = happy()
+print(obj.a)
+obj.method()'''
+
+#protected (_a) i.e using single underscore before the variable makes it protected
+'''class encap():
+    _a=10
+    c=11
+    def encapfunction(self):
+        _b=12
+        print("encapfuntion")
+        print(_b)
+obj = encap()
+print(obj._a)
+obj.encapfunction()
+print(obj.c)'''
+
+#private (__a)
+'''class encap():
+    __a = 10
+    print(__a)
+    def encapfunction(self):
+        print("encapfunction")
+        print(self.__a)
+obj = encap()
+obj.encapfunction()
+print(obj.__a)'''
+
+
+
+        
+
